815616b12ef9f52a07fc56852591b6a2e5900a15
jaredmontoya <49511278+jaredmontoya@users.noreply.github.com>
2024-11-07 16:47:14 +0100

    flake: remove dependency on flake-utils,devshells and replace pre-commit hooks with treefmt-nix

--- flake.nix
+++ flake.nix
@@ -2,88 +2,81 @@
   description = "🍁 Generate infrastructure and network diagrams directly from your NixOS configurations";
 
   inputs = {
-    devshell = {
-      url = "github:numtide/devshell";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
 
-    pre-commit-hooks = {
-      url = "github:cachix/pre-commit-hooks.nix";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.nixpkgs-stable.follows = "nixpkgs";
     };
   };
 
-  outputs = {
-    self,
-    devshell,
-    flake-utils,
-    nixpkgs,
-    pre-commit-hooks,
-    ...
-  } @ inputs:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      systems,
+      treefmt-nix,
+      ...
+    }@inputs:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs (import systems);
+
+      treefmtEval = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        treefmt-nix.lib.evalModule pkgs ./treefmt.nix
+      );
+    in
     {
       flakeModule = ./flake-module.nix;
 
       # Expose NixOS module
-      nixosModules.topology = ./nixos/module.nix;
-      nixosModules.default = self.nixosModules.topology;
+      nixosModules = {
+        default = self.nixosModules.topology;
+        topology = ./nixos/module.nix;
+      };
 
       # A nixpkgs overlay that adds the parametrized builder as a package
-      overlays.default = self.overlays.topology;
-      overlays.topology = import ./pkgs/default.nix;
-    }
-    // flake-utils.lib.eachDefaultSystem (system: rec {
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [
-          self.overlays.default
-          devshell.overlays.default
-        ];
-      };
-
-      packages.docs = pkgs.callPackage ./pkgs/docs.nix {
-        flakeInputs = inputs;
-        flakeOutputs = self;
-      };
-
-      # `nix flake check`
-      checks.pre-commit-hooks = pre-commit-hooks.lib.${system}.run {
-        src = nixpkgs.lib.cleanSource ./.;
-        hooks = {
-          # Nix
-          alejandra.enable = true;
-          deadnix.enable = true;
-          statix.enable = true;
-        };
-      };
-
-      # `nix develop`
-      devShells.default = pkgs.devshell.mkShell {
-        name = "nix-topology";
-
-        commands = [
-          {
-            package = pkgs.alejandra;
-            category = "formatters";
-          }
-          {
-            package = pkgs.deadnix;
-            category = "linters";
-          }
-          {
-            package = pkgs.statix;
-            category = "linters";
-          }
-        ];
-
-        devshell.startup.pre-commit.text = self.checks.${system}.pre-commit-hooks.shellHook;
+      overlays = {
+        default = self.overlays.topology;
+        topology = import ./pkgs/default.nix;
       };
 
       # `nix fmt`
-      formatter = pkgs.alejandra;
-    });
+      formatter = forAllSystems (system: treefmtEval.${system}.config.build.wrapper);
+
+      # `nix flake check`
+      checks = forAllSystems (system: {
+        formatting = treefmtEval.${system}.config.build.check self;
+      });
+
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          docs = pkgs.callPackage ./pkgs/docs.nix {
+            flakeInputs = inputs;
+            flakeOutputs = self;
+          };
+        }
+      );
+
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              self.overlays.default
+            ];
+          };
+        in
+        import ./shell.nix { inherit pkgs; }
+      );
+    };
 }
